89dd2b2b923ef529aea92eeb4551c1caf055ace9
amusarri <186473330+amusarri@users.noreply.github.com>
2024-12-30 20:22:58 +0800

    Altered DOM.

--- jsfile.js
+++ jsfile.js
@@ -1,7 +1,11 @@
 const container = document.querySelector("#container");
 
-const content = document.createElement("div");
+const content = document.createElement("p");
 content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+content.textContent = "Hi, I'm red";
+content.style.color = "red";
 
 container.appendChild(content);
+
+//const contentTwo = document.createElement("h3")
+//content.classList.add("contentTwo")
