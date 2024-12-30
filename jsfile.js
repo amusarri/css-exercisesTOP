ec704051b4cb4986e8849d0bf376e00be2de91f8
amusarri <186473330+amusarri@users.noreply.github.com>
2024-12-30 20:38:10 +0800

    Altered DOM.

--- jsfile.js
+++ jsfile.js
@@ -7,5 +7,19 @@ content.style.color = "red";
 
 container.appendChild(content);
 
-//const contentTwo = document.createElement("h3")
-//content.classList.add("contentTwo")
+
+const contentTwo = document.createElement("h3");
+contentTwo.classList.add("contentTwo");
+contentTwo.textContent = "I'm a blue h3!";
+contentTwo.style.color = "blue";
+
+container.appendChild(contentTwo);
+
+
+//const contentThree = document.createElement("div");
+//contentThree.classList.add("contentThree");
+//contentThree.style.backgroundColor = "pink";
+//contentThree.style.borderColor = "black"
+//contentThree.appendChild("h1") 
+
+  
